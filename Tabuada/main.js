f8bc5e8f80659e147707bd7835d184bc18857b77
João Vitor Bortoliero <bortoliero.10@hotmail.com>
2023-04-06 20:42:02 -0300

    Update main.js

--- Tabuada/main.js
+++ Tabuada/main.js
@@ -1,31 +1,29 @@
-const botao = document.getElementById('botao');
+const botao = document.getElementById('button1');
 const resultado = document.getElementById('resultado');
 
+
 botao.addEventListener('click', () => {
-    resultado.innerHTML = " ";
     const num = document.getElementById('num').value;
     if (num != 0 || num > 0) {
         for (let i = 1; i < 10; i++) {
             let texto = `${num} x ${i} = ${num * i} <br>`;
+            resultado.innerHTML += texto;
         }
-        resultado.appendChild(texto);
     } else {
         window.alert("O campo está vazio, Insira um valor!");
     }
 });
 
-
-const botao2 = document.getElementById('botao2');
-
-var contador = 0;
+const botao2 = document.getElementById('button2');
+let contador = 0;
 
 botao2.addEventListener('click', () => {
     contador++;
     document.getElementById('cliques').innerHTML = contador;
-})
+});
         
 
-const botao3 = document.getElementById('botao3');
+const botao3 = document.getElementById('button3');
 
 botao3.addEventListener('click', () => {
     const min = document.getElementById('min').value;
